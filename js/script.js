f65441bde47b2fded19bd1c2829c478f7298a63e
25557 <43168595+25557@users.noreply.github.com>
2019-05-07 10:57:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // test
+  $(".splitchar").splitchar();
+  // $('body').hide(3000).delay(1000).show(500);
+  // muis events
+  $('figure img').mouseenter(function() {
+    $(this).fadeTo(600, .1);
+  }).mouseleave(function() {
+    $(this).fadeTo(1000, 1);
+  }).click(function() {
+    $(this).next().toggle(700);
+  })
+
+  // footer
+
+  $('footer').click(function() {
+    $(this).animate({
+      top: '-=20px',
+      opacity: '-=.2'
+    }, 900)
+  }).dblclick(function() {
+    $(this).animate({
+      top: '0',
+      opacity: '1'
+    }, 900)
+  });
+});
